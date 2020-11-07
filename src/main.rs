40c2bf9f59df54234dc2dc7fd3ca97b738d1fbd3
AleBuser <alebuser.98@gmail.com>
2020-11-07 12:01:34 +0100

    implement channel_switch and bundle messages

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gateway_core::gateway::publisher::Channel;
 use local::device_auth::keystore::KeyManager;
-use local::types::config::Config;
+use local::types::{channel_state::ChannelState, config::Config};
 use local::wifi_connectivity::http_server;
 
 use std::fs::File;
@@ -11,28 +11,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     //read configuration file
     let config: Config = serde_json::from_reader(File::open("config.json").unwrap()).unwrap();
 
-    let store = KeyManager::new(config.whitelisted_device_ids);
+    let store = KeyManager::new(config.whitelisted_device_ids.clone());
 
     println!("Starting....");
 
-    let channel = Arc::new(Mutex::new(Channel::new(
-        config.node,
-        config.mwm,
-        config.local_pow,
-        None,
-    )));
-    let (addr, msg_id) = match channel.lock().expect("").open() {
+    let mut channel = Channel::new(config.node.clone(), config.mwm, config.local_pow, None);
+    let (addr, msg_id) = match channel.open() {
         Ok(a) => a,
         Err(_) => panic!("Could not connect to IOTA Node, try with another node!"),
     };
-
     let channel_id = format!("{}:{}", addr, msg_id);
-    println!("Channel root: {:?}", channel_id);
-    println!(
-        "\n To read the messages copy the channel root into https://explorer.iot2tangle.io/ \n "
-    );
+
+    let channel_state = Arc::new(Mutex::new(ChannelState {
+        channel: channel,
+        channel_id: channel_id,
+    }));
 
     let store = Arc::new(Mutex::new(store));
 
-    http_server::start(config.port, channel, store, channel_id).await
+    http_server::start(config, channel_state, store).await
 }
---
+++ src/types/channel_state.rs
@@ -0,0 +1,6 @@
+use gateway_core::gateway::publisher::Channel;
+
+pub struct ChannelState {
+    pub channel: Channel,
+    pub channel_id: String,
+}
